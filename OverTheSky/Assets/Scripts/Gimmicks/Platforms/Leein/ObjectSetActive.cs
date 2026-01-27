94d91ddbef15b650fd7133dc46daa57ff4bfd1ac
sadsmile <phycoeco@naver.com>
2026-01-27 16:46:29 +0900

    feat/BreakablePlatform
    기본 발판인 BreakablePlatform제작
    
    1.밟으면 1.5초 뒤에 사라짐
    2.1.5사이동안 색 변경
    3. 3초후 오브젝트 다시 활성화

---
+++ OverTheSky/Assets/Scripts/Gimmicks/Platforms/Leein/ObjectSetActive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSetActive : MonoBehaviour
+{
+     public GameObject myObject;
+    WaitForSeconds seconds;
+    public void ActiveSelf(float time)
+    {
+        StartCoroutine(DelayActive(time));
+    }
+
+    private IEnumerator DelayActive(float time)
+    {
+        if(seconds ==null)
+        {
+            seconds=new WaitForSeconds(time);
+        }
+        yield return seconds;
+        myObject.SetActive(true);
+    }
+}
