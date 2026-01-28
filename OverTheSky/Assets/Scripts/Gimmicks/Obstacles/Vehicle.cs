3577c035421f90e1681229f321f36f51e944d2a6
Crowasang <bighyun620@naver.com>
2026-01-28 15:48:07 +0900

    feat:차가플레이어가 치이면 날라가는 기능 구현

--- OverTheSky/Assets/Scripts/Gimmicks/Obstacles/Vehicle.cs
+++ OverTheSky/Assets/Scripts/Gimmicks/Obstacles/Vehicle.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    private float _moveSpeed = 100f;
-
-    private void Update()
+    private float _moveSpeed = 50f;
+    private float _pushForce = 100f;
+    private float _upForce = 10f;
+    
+    private void FixedUpdate()
     {
-        gameObject.transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
-        Destroy(gameObject, 0.3f);
+        transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+        Destroy(gameObject, 1f);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody player = other.gameObject.GetComponent<Rigidbody>();
+
+            if (player != null)
+            {
+                player.freezeRotation = true;
+                
+                player.AddForce(transform.forward * _pushForce, ForceMode.VelocityChange);
+                player.AddForce(Vector3.up * _upForce, ForceMode.VelocityChange);
+            }
+        }
     }
 }
