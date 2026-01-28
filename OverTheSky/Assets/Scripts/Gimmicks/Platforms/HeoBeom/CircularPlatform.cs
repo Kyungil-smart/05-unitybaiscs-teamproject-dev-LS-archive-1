fad7b0f9307448fd47a2175b191ffc048c6a8225
Nocdamso <adsnmf@gmail.com>
2026-01-28 17:43:59 +0900

    feat : RotatingObstacle
    회전 장애물 만들었고 충돌처리가 어려워서 아직 못 했습니다.

--- OverTheSky/Assets/Scripts/Gimmicks/Platforms/HeoBeom/CircularPlatform.cs
+++ OverTheSky/Assets/Scripts/Gimmicks/Platforms/HeoBeom/CircularPlatform.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircularPlatform : MonoBehaviour
+namespace OverTheSky.Gimmicks.Platforms
 {
-    [SerializeField][Range(0, 200)] private float _rotateSpeed;
-    [SerializeField] private GameObject _pivot;
-
-    private void FixedUpdate()
+    public class CircularPlatform : MonoBehaviour
     {
-        Rotate();
-    }
+        [SerializeField][Range(0, 200)] private float _rotateSpeed;
+        [SerializeField] private GameObject _pivot;
 
-    private void Rotate()
-    {
-        // pivot의 Y축을 기준으로 다른 오브젝트가 공전을 할 수 있도록 함
-        transform.RotateAround(_pivot.transform.position, Vector3.up, _rotateSpeed * Time.deltaTime);
+        private void FixedUpdate()
+        {
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            // pivot의 Y축을 기준으로 다른 오브젝트가 공전을 할 수 있도록 함
+            transform.RotateAround(_pivot.transform.position, Vector3.up, _rotateSpeed * Time.deltaTime);
+        }
     }
 }
--- OverTheSky/Assets/Scripts/Gimmicks/Platforms/HeoBeom/PingPongPlatform.cs
+++ OverTheSky/Assets/Scripts/Gimmicks/Platforms/HeoBeom/PingPongPlatform.cs
@@ -3,44 +3,48 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PingPongPlatform : MonoBehaviour
+namespace OverTheSky.Gimmicks.Platforms
 {
-    [SerializeField][Range(0, 30)] private float _moveSpeed;
-    [SerializeField]private Transform _targetPointA;
-    [SerializeField]private Transform _targetPointB;
-
-    private Transform _currentLocate;
-
-    private void Start()
+    public class PingPongPlatform : MonoBehaviour
     {
-        // 시작 시 이동할 위치를 정해줌
-        _currentLocate = _targetPointA;
-    }
-    
-    private void FixedUpdate()
-    {
-        Move();
-    }
+        [SerializeField][Range(0, 30)] private float _moveSpeed;
+        [SerializeField]private Transform _targetPointA;
+        [SerializeField]private Transform _targetPointB;
 
-    private void Move()
-    {  
-        // 널 레퍼런스 익셉션 오류가 떠서 null이 아닐 때만 실행
-        if(_currentLocate != null)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _currentLocate.position, _moveSpeed * Time.deltaTime);
-        }
-    }
+        private Transform _currentLocate;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        // PingPongTargetA와 충돌 시 _targetPointB로 이동
-        if (other.name == "PingPongTargetA")
+        private void Start()
         {
-            _currentLocate = _targetPointB;
-        }
-        else if (other.name == "PingPongTargetB")
-        {
+            // 시작 시 이동할 위치를 정해줌
             _currentLocate = _targetPointA;
+        }
+    
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {  
+            // 널 레퍼런스 익셉션 오류가 떠서 null이 아닐 때만 실행하도록
+            if(_currentLocate != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _currentLocate.position, _moveSpeed * Time.deltaTime);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // 'PingPongTargetA'와 충돌 시 _targetPointB로 이동
+            if (other.name == "PingPongTargetA")
+            {
+                _currentLocate = _targetPointB;
+            }
+            // 'PingPongTargetB'와 충돌 시 _targetPoinA로 이동
+            else if (other.name == "PingPongTargetB")
+            {
+                _currentLocate = _targetPointA;
+            }
         }
     }
 }
