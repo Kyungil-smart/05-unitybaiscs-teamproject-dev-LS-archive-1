6ad275f0b951775623e713a1f6681fdd2560e4af
sadsmile <phycoeco@naver.com>
2026-01-29 14:45:47 +0900

    feat/DissolveDoor
    특정 아이템을 먹으면 객체에 등록된 이벤트 작동 이벤트는 문 열수 있게 하는 것

---
+++ OverTheSky/Assets/Scripts/Gimmicks/Platforms/Leein/Other/DissolveKey.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveKey : MonoBehaviour
+{
+    [SerializeField] private DissolveController _dissolveController;
+
+     
+
+    private  delegate void MyAction();
+    private MyAction _triggerAction; 
+
+    private void Awake()
+    {
+        _triggerAction = _dissolveController.CanOpenDoor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            _triggerAction?.Invoke();
+            this.gameObject.SetActive(false);
+        }
+    }
+}
