0aeb21a4fc9b9253a054ab5953356c735b469f3b
sadsmile <phycoeco@naver.com>
2026-01-30 17:16:57 +0900

    feat/SkyboxChange
    플레이어 높이  기준으로 스카이박스 보간 교체

---
+++ OverTheSky/Assets/Scripts/Gimmicks/Platforms/Leein/TestCode/TestSurface/Change.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+    public LayerMask LayerMask;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            var data= other.gameObject.GetComponent<Surface>();
+            data.OnChangeData(LayerMask);
+        }
+
+
+         
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var data = other.gameObject.GetComponent<Surface>();
+            data.OnExitData();
+        }
+    }
+}
