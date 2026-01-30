cfa06ca22969abda1c1f65b61e4cbe9681d171b6
dev-LS-archive <gyu3941@gmail.com>
2026-01-30 17:42:22 +0900

    feat: 매니저 및 컨트롤러 일부 수정

--- OverTheSky/Assets/Scripts/Core/Managers/GameManager.cs
+++ OverTheSky/Assets/Scripts/Core/Managers/GameManager.cs
@@ -2,6 +2,7 @@ namespace OverTheSky.Core
 {
     public class GameManager : Singleton<GameManager>
     {
+        
         void Start()
         {
             Logger.Instance.LogInfo("Starting Game Manager");
